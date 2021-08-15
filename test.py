101d1f2f7dfcba62c29d3687b599ba3e6ed2336c
nschang <17569847+nschang@users.noreply.github.com>
2021-08-15 23:49:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,16 @@
 # test network structure
 # -----------------------------------------------------------
 from keras.layers import Input
-
 from nets.yolo4 import yolo_body
 
 if __name__ == "__main__":
-    inputs = Input([416, 416, 3])
-    model = yolo_body(inputs, 3, 80)
+    input_shape     = Input([416, 416, 3])
+    #input_shape     = [416, 416, 3]
+    #anchors_mask    = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
+    anchors_mask    = 3
+    num_classes     = 80
+    model           = yolo_body(input_shape, anchors_mask, num_classes)
     model.summary()
 
-    # for i,layer in enumerate(model.layers):
-    #     print(i,layer.name)
+    for i,layer in enumerate(model.layers):
+        print(i,layer.name)
